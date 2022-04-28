4863bf8ff46f7af16042fd05788d3fa4a8356af4
Connor Alexander <connorschumer@gmail.com>
2022-04-28 13:46:13 +1000

    created the skeleton of project. No styling--though none required.

--- main.js
+++ main.js
@@ -1,1 +1,59 @@
-//code goes here
+// Importing prompts
+import prompts from "./src/prompts.js";
+
+
+// helper function
+const getObject = () => {
+    // generate array ID that is random and won't exceed the length of the array
+
+    const id = Math.floor(Math.random() * prompts.objects.length);
+    
+    // pickup an array element with the random id
+    const newObject = prompts.objects[id];
+    
+    // output the element
+    return newObject;
+    
+}
+
+// helper function
+const getAction = () => {
+    // generate array ID that is random and won't exceed the length of the array
+    const id = Math.floor(Math.random() * prompts.actions.length);
+
+    // pickup an array element with the random id
+    const newAction = prompts.actions[id];
+
+    // output the element
+    return newAction;
+}
+
+// helper function
+const getPunctuation = () => {
+    // generate array ID that is random and won't exceed the length of the array
+    const id = Math.floor(Math.random() * prompts.punctuation.length);
+
+    // pickup an array element with the random id
+    const newPunct = prompts.punctuation[id];
+
+    // output the element
+    return newPunct;
+}
+
+// higher-order function
+const makeHeadline = () => {
+    // generate
+    let headline = getObject() + ' ' + getAction() + ' ' + getObject() + getPunctuation();
+    headline = headline.toUpperCase();
+
+    // output
+    console.log(headline);
+    return headline;
+}
+
+// event listener
+const button = document.getElementById('genButton');
+const headline = document.getElementById('headline');
+button.onclick = () => {
+    headline.innerText = makeHeadline();
+};
---
+++ src/prompts.js
@@ -0,0 +1,7 @@
+const prompts = {
+    objects: ["pineapple", "bongo drum", "man", "murder weapon", "banana", "Nissan Pathfinder", "neighbourhood villain", "old lady", "fly", "tumbleweed", "prime minister", "president", "mars", "jupiter", "brick-layer", "Rowan"],
+    actions: ["wishes to be", "eats", "becomes", "stole", "shouldn't", "consumes", "TKO'd", "outdrinks", "wealthier than", "says", "prays to", "without", "unworthy of"],
+    punctuation: ["!", ".", ".", ".", "?"]
+};
+
+export default prompts;
